39489583c74a2b311045d1938d11a19867a16905
JohannaR11 <johannareboucas11@gmail.com>
2021-11-20 21:27:02 -0400

    atualizando pagina de busca de personagens

--- rick-morty-react/src/Pages/Personagens/index.js
+++ rick-morty-react/src/Pages/Personagens/index.js
@@ -1,27 +1,31 @@
-import { useEffect, useState } from "react";
+import {useState} from "react";
 import HeaderResponsive from "../../Components/HeaderResponsive";
 import api from "../../Api";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import "./styles.css";
+import Card from "../../Components/Card";
 
 function Personagens() {
 
     const [data, setData] = useState(null);
     const [name, setName] = useState("");
 
-    useEffect(() => {
-        // async function loadAll() {
-        //     let res = await api.getPersonagemByName();
-        //     setData(res.data)
-        //     console.log(res.data)
-        // }
-        // loadAll();
-    }, []);
+    async function load() {
+        let res = await api.getPersonagemByName(name);
+        setData(res)
+    }
 
-    function clickMouse(event) {
+    async function searchCharacter(event) {
         event.preventDefault();
-        console.log("Escolhendo o Nome:", name)
-        // let res = await api.getPersonagemByName(name);
+        setData(null);
+        if (name) {
+            load();
+        }
+    }
+
+    function handleName(event){
+        setName (event.target.value);
     }
 
 
@@ -30,20 +34,30 @@ function Personagens() {
         <div>
             <HeaderResponsive />
             <div className="search-container">
-                <form onSubmit={clickMouse}>
-                    <input onChange={(event) => {
-                        setName(event.target.value);
-                    }
-                    } />
+                <form onSubmit={searchCharacter}>
+                    <input onChange={handleName} />
                     <button type="submit">
                         <FontAwesomeIcon icon={faSearch} size="lg" />
                     </button>
                 </form>
             </div>
-
+            <div className="results-container">
+            {data &&
+                    ((data.error)
+                        ? <h1>Busca não encontrada</h1>
+                        : data.results.map((item, key) => {
+                            return <Card
+                                key={key}
+                                id={item.id}
+                                img={item.image}
+                                name={item.name}
+                            />
+                        })
+                    )
+                }
+            </div>
         </div>
 
     );
 }
-
 export default Personagens;
